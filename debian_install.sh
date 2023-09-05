8c4d745dd794e5a345d7e24a0153f8a76eb21b01
Stid <111974729+Stidgit@users.noreply.github.com>
2023-09-05 18:22:12 +0300

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -28,4 +28,13 @@ sudo apt install -y neofetch firefox-esr lxappearance feh neovim btop wget lutri
 sudo apt install -y virt-manager qemu-system
 
 # Window manager
-sudo apt install -y i3-wm suckless-tools sxhkd polybar
+sudo apt install -y i3-wm suckless-tools sxhkd polybar rofi unzip dunst libnotify-bin
+
+# Install Lightdm Console Display Manager
+sudo apt install -y lightdm lightdm-gtk-greeter-settings
+sudo systemctl enable lightdm
+
+# Clean
+sudo apt autoremove
+
+printf "\e[1;32mFinished Installing, You may reboot.\e[0m\n"
