e41b5e1d23c7fc1a249e53c3a2983b2d33b23526
Stid <111974729+Stidgit@users.noreply.github.com>
2023-09-08 22:06:04 +0300

    Update void_base.sh

--- void_base.sh
+++ void_base.sh
@@ -2,5 +2,8 @@
 
 # Audio
 sudo xbps-install -Syu
-sudo xbps-install alsa-utils pulseaudio pavucontrol
+xbps-install alsa-utils pipewire alsa-pipewire
 sudo ln -s /etc/sv/alsa /var/service && sudo ln -s /etc/sv/pulseaudio /var/service
+
+# Enable repositories
+xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
