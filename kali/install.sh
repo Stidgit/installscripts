af80f0177d51d622be4928029169c7c07f31fd91
Stidgit <alfred.xueke@gmail.com>
2023-10-24 20:20:50 +0300

    improved install.sh

--- kali/install.sh
+++ kali/install.sh
@@ -2,9 +2,27 @@
 
 # Check if running as root
 if [ "$EUID" -ne 0 ]; then
-    echo "run as root."
-    exit
+    echo "Please run as root."
+    exit 1
 fi
 
-apt update
-apt install -y i3 terminator ranger rofi nitrogen feh compton build-essential neovim
+# Update the package list
+if ! apt update; then
+    echo "Failed to update package list. Exiting."
+    exit 1
+fi
+
+# Install all packages at once
+if ! apt install -y i3 \
+                    terminator \
+                    rofi \
+                    nitrogen \
+                    feh \
+                    compton \
+                    build-essential \
+                    btop \
+                    ranger \
+                    neovim; then
+    echo "Failed to install packages. Exiting."
+    exit 1
+fi
